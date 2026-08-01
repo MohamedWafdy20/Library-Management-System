57f19611ebeaef21f86c3ec2dc2ca6eef7faaf5f
shosh-code <shimaaahmedelian@gmail.com>
2026-08-01 06:01:40 +0300

    Initial project setup with Book model and sample data

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'publish_year', 'avilable_copies', 'isbn','total_copies')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('category', 'publish_year')
 
 # Register your models here.
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Book Title")
+    author = models.CharField(max_length=100, verbose_name="Author Name")
+    isbn = models.CharField(max_length=13, unique=True,)
+    category = models.CharField(max_length=50, verbose_name="Category")
+    publish_year = models.IntegerField(verbose_name="Publish Year")
+    avilable_copies = models.PositiveIntegerField(default=1, verbose_name="Avilable Copies")
+    total_copies = models.PositiveIntegerField(default=1, verbose_name="Total Copies")
+    description = models.TextField(blank=True, null=True, verbose_name="Description")
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
